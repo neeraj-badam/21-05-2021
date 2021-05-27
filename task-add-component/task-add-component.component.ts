515eb44e570b6f395c7880a8965b86a18cbd2e58
neeraj-badam <69072023+neeraj-badam@users.noreply.github.com>
2021-05-27 14:53:15 +0530

    Create task-add-component.component.ts

---
+++ task-add-component/task-add-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-add-component',
+  templateUrl: './task-add-component.component.html',
+  styleUrls: ['./task-add-component.component.css']
+})
+export class TaskAddComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
